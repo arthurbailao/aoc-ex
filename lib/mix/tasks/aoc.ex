f355bffeb46f3c73cafbbc1cb4697f8e75903b68
Arthur Bailão <arthurbailao@gmail.com>
2024-12-05 03:57:50 -0300

    fix: fixing mix task

--- lib/mix/tasks/aoc.ex
+++ lib/mix/tasks/aoc.ex
@@ -9,20 +9,18 @@ defmodule Mix.Tasks.Aoc do
     {parsed, _, _} =
       OptionParser.parse(args, strict: [year: :integer, day: :integer, file: :string])
 
-    solve(parsed)
-  end
+    with {:ok, year} <- Keyword.fetch(parsed, :year),
+         {:ok, day} <- Keyword.fetch(parsed, :day),
+         {:ok, file} <- Keyword.fetch(parsed, :file) do
+      day_module = "Elixir.AocEx.Year#{year}.Day#{day}" |> String.to_existing_atom()
+      input = File.read!(file)
 
-  defp solve(year: year, day: day, file: file) do
-    day_module = "Elixir.AocEx.Year#{year}.Day#{day}" |> String.to_existing_atom()
-
-    input = File.read!(file)
-
-    apply(day_module, :first, [input]) |> IO.puts()
-    apply(day_module, :second, [input]) |> IO.puts()
-  end
-
-  defp solve(_) do
-    IO.puts("Usage: mix aoc --year <year> --day <day> --file <file>")
-    exit({:shutdown, 126})
+      apply(day_module, :first, [input]) |> IO.puts()
+      apply(day_module, :second, [input]) |> IO.puts()
+    else
+      :error ->
+        IO.puts("Usage: mix aoc --year <year> --day <day> --file <file>")
+        exit({:shutdown, 126})
+    end
   end
 end
